ef6234c7c2d3fc719b5fac3b37151344ab7c1fed
Rohit Jain <50474637+rohit-abax@users.noreply.github.com>
2020-12-31 16:53:57 +0530

    Remove dummy data

--- src/app.js
+++ src/app.js
@@ -13,36 +13,36 @@ import getVisibleExpenses from './selectors/expenses';
 const store = configureStore();
 
 const unsubscribe = store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
+    // const state = store.getState();
+    // const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log("Test Comment");
 });
 //description: 'Rent', amount: 100, createdAt: -1000
-const expense1 = store.dispatch(
-    addExpense({
-        description: 'Water Bill',
-        note: 'This is a test expense',
-        amount: 200,
-        createdAt: 1606804200000 ,
-    })
-);
-store.dispatch(
-    addExpense({
-        description: 'Gas Bill',
-        note: 'This is another test expense',
-        amount: 50000,
-        createdAt: 1607063400000  ,
-    })
-);
+// const expense1 = store.dispatch(
+//     addExpense({
+//         description: 'Water Bill',
+//         note: 'This is a test expense',
+//         amount: 200,
+//         createdAt: 1606804200000 ,
+//     })
+// );
+// store.dispatch(
+//     addExpense({
+//         description: 'Gas Bill',
+//         note: 'This is another test expense',
+//         amount: 50000,
+//         createdAt: 1607063400000  ,
+//     })
+// );
 
-store.dispatch(
-    addExpense({
-        description: 'Rent',
-        note: 'This is another test expense',
-        amount: 5000,
-        createdAt: 1609050600000  ,
-    })
-);
+// store.dispatch(
+//     addExpense({
+//         description: 'Rent',
+//         note: 'This is another test expense',
+//         amount: 5000,
+//         createdAt: 1609050600000  ,
+//     })
+// );
 
 //console.log('removing expense1',expense1);
 //store.dispatch(removeExpense(expense1.expense.id));
